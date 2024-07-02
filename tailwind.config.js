353e950edbae8f58052bdd41b5f0e239e4bfd698
Archit Kulshrestha <u22ch@051@ched.svnit.ac.in>
2024-07-03 04:23:21 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      colors: {
+        text: "#0f1714",
+        background: "#f8faf9",
+        primary: "#699d8a",
+        secondary: "#a5bac4",
+        accent: "#8aa1b2",
+      },
     },
   },
   plugins: [],
